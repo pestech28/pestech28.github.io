f94b9841b4105e327dcb5588ec76ab5f3b461453
pestech28 <pestech28@yahoo.com>
2026-08-07 17:46:22 -0400

    Add files via upload

---
+++ back/digivision/v4/cable.js
@@ -0,0 +1,81 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index4.html", "NULL"],
+        ["index2.html#https://sweet.tv/sk/tv/847-jednotka-hd", "https://pestech28.github.io/back/digivision/v4/ch1.png"],
+        ["index2.html#https://sweet.tv/sk/tv/848-dvojka-hd", "https://pestech28.github.io/back/digivision/v4/ch2.png"],
+        ["index2.html#https://media.joj.sk/embed/3Yz1gBQeKTs?autoplay=1", "https://pestech28.github.io/back/digivision/v4/ch3.png"],
+        ["index2.html#https://media.joj.sk/embed/DKZwKhUlV3i?autoplay=1", "https://pestech28.github.io/back/digivision/v4/ch4.png"],
+        ["index3.html#https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8", "https://pestech28.github.io/back/digivision/v4/ch5.png"],
+        ["index2.html#https://media.cms.markiza.sk/embed/markiza-live?autoplay=any", "https://pestech28.github.io/back/digivision/v4/ch6.png"],
+        ["index2.html#https://media.cms.markiza.sk/embed/doma-live?autoplay=any", "https://pestech28.github.io/back/digivision/v4/ch7.png"],
+        ["index2.html#https://media.cms.markiza.sk/embed/dajto-live?autoplay=any", "https://pestech28.github.io/back/digivision/v4/ch8.png"],
+        ["index2.html#https://media.cms.markiza.sk/embed/klasik-live?autoplay=any", "https://pestech28.github.io/back/digivision/v4/ch9.png"],
+        ["index3.html#https://live.cdn.joj.sk/live/andromeda/cs_history-1080.m3u8", "https://pestech28.github.io/back/digivision/v4/ch10.png"],
+        ["index3.html#https://live.cdn.joj.sk/live/andromeda/cs_film-1080.m3u8", "https://pestech28.github.io/back/digivision/v4/ch11.png"],
+        ["index3.html#https://live.cdn.joj.sk/live/andromeda/cs_mystery-1080.m3u8", "https://pestech28.github.io/back/digivision/v4/ch12.png"],
+        ["index2.html#https://www.ta3.com/live", "https://pestech28.github.io/back/digivision/v4/ch13.png"],
+        ["index2.html#https://pluto.tv/us/watch/live-tv/#18785", "https://pestech28.github.io/back/digivision/v4/ch14.png"],
+        ["index2.html#https://pestech28.github.io/back/digivision/v4/yt/thehill.html", "https://pestech28.github.io/back/digivision/v4/ch15.png"],
+        ["index2.html#https://i1.weatherstar.dev/", "https://pestech28.github.io/back/digivision/v4/ch16.png"],
+        ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://pestech28.github.io/back/digivision/v4/ch17.png"],
+        ["index3.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-xHCbpk-playlist.m3u8", "https://pestech28.github.io/back/digivision/v4/ch18.png"],
+        ["index2.html#https://pluto.tv/us/watch/live-tv/#18785", "https://pestech28.github.io/back/digivision/v4/ch19.png"],
+        ["index2.html#https://pestech28.github.io/back/digivision/v4/yt/tao.html", "https://pestech28.github.io/back/digivision/v4/ch20.png"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
+
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Jednotka", "Všeobecný obsah"],
+        ["Dvojka", "Všeobecný a vzdelávací obsah"],
+        ["TV JOJ", "Televízne programy a filmy"],
+        ["JOJ Plus", "Americké televízne relácie"],
+        ["JOJ 24", "Správy 24 hodín denne, 7 dní v týždni"],
+        ["Marzika", "Rodinný obsah"],
+        ["Doma", "Televízne relácie pre ženy"],
+        ["Dajto", "Televízne relácie pre mužov"],
+        ["Klasik", "Klasické televízne relácie"],
+        ["CS History", "Historický kanál"],
+        ["CS Film", "Filmový kanál"],
+        ["CS Mystery", "Mysteriózne filmy"],
+        ["TA3", "Správy 24 hodín denne, 7 dní v týždni"],
+        ["Bob Ross", "Joy of Painting"],
+        ["The Hill", "Non bias news 24/7"],
+        ["Weather Scan", "Weather info 24/7"],
+        ["Cartoon Network", "Cartoons"],
+        ["Toonami Aftermath", "Cartoons and Anime"],
+        ["00s Replay", "Movies from the new mellenium"],
+        ["Zen Music", "Traditional Asian music 24/7"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
+
+function getwebchannels() {
+    let channeldb = [
+        ["https://sweet.tv/sk/tv/847-jednotka-hd", "https://pestech28.github.io/back/digivision/v4/ch1.png"],
+        ["https://sweet.tv/sk/tv/848-dvojka-hd", "https://pestech28.github.io/back/digivision/v4/ch2.png"],
+        ["https://media.joj.sk/embed/3Yz1gBQeKTs?autoplay=1", "https://pestech28.github.io/back/digivision/v4/ch3.png"],
+        ["https://media.joj.sk/embed/DKZwKhUlV3i?autoplay=1", "https://pestech28.github.io/back/digivision/v4/ch4.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8", "https://pestech28.github.io/back/digivision/v4/ch5.png"],
+        ["https://media.cms.markiza.sk/embed/markiza-live?autoplay=any", "https://pestech28.github.io/back/digivision/v4/ch6.png"],
+        ["https://media.cms.markiza.sk/embed/doma-live?autoplay=any", "https://pestech28.github.io/back/digivision/v4/ch7.png"],
+        ["https://media.cms.markiza.sk/embed/dajto-live?autoplay=any", "https://pestech28.github.io/back/digivision/v4/ch8.png"],
+        ["https://media.cms.markiza.sk/embed/klasik-live?autoplay=any", "https://pestech28.github.io/back/digivision/v4/ch9.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/cs_history-1080.m3u8", "https://pestech28.github.io/back/digivision/v4/ch10.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/cs_film-1080.m3u8", "https://pestech28.github.io/back/digivision/v4/ch11.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/cs_mystery-1080.m3u8", "https://pestech28.github.io/back/digivision/v4/ch12.png"],
+        ["https://www.ta3.com/live", "https://pestech28.github.io/back/digivision/v4/ch13.png"],
+        ["https://pluto.tv/us/watch/live-tv/#18785", "https://pestech28.github.io/back/digivision/v4/ch14.png"],
+        ["https://pestech28.github.io/back/digivision/v4/yt/thehill.html", "https://pestech28.github.io/back/digivision/v4/ch15.png"],
+        ["https://i1.weatherstar.dev/", "https://pestech28.github.io/back/digivision/v4/ch16.png"],
+        ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://pestech28.github.io/back/digivision/v4/ch17.png"],
+        ["index2.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-xHCbpk-playlist.m3u8", "https://pestech28.github.io/back/digivision/v4/ch18.png"],
+        ["https://pluto.tv/us/watch/live-tv/#18785", "https://pestech28.github.io/back/digivision/v4/ch19.png"],
+        ["https://pestech28.github.io/back/digivision/v4/yt/tao.html", "https://pestech28.github.io/back/digivision/v4/ch20.png"],
+    ];
+    return channeldb;
+}
