e4d691f7ba2bacd3444895ff53a38473d58cb443
pestech28 <pestech28@yahoo.com>
2026-08-17 22:38:34 -0400

    Add files via upload

--- back/digivision/v3r5054/cable.js
+++ back/digivision/v3r5054/cable.js
@@ -1,1 +1,92 @@
+﻿function getchannels() {
+    let channeldb = [
+        ["index4.html", "NULL"],
+        ["index3.html#https://streaming-live.rtp.pt/liverepeater/rtp1HD.smil/playlist.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch1.png"],
+        ["index3.html#https://streaming-live.rtp.pt/liverepeater/rtp2HD.smil/playlist.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch2.png"],
+        ["index3.html#https://streaming-live.rtp.pt/livetvhlsDVR/rtpnHDdvr.smil/playlist.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch3.png"],
+        ["index2.html#https://sic.pt/direto", "https://pestech28.github.io/back/digivision/v3r5054/ch4.png"],
+        ["index3.html#https://video-auth7.iol.pt/live_tvi/live_tvi/edge_servers/tvi-720p/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8xOC8yMDI2IDEyOjEyOjU4IEFNJmhhc2hfdmFsdWU9UXdEL0ViVWpjMWx1VHR2S0UrWm00QT09JnZhbGlkbWludXRlcz0xNDQwJmlkPTZhMzViODhjLTBjOWQtNDZhMy05MmFjLTc3YmZkMjI3NTg0Yg==", "https://pestech28.github.io/back/digivision/v3r5054/ch5.png"],
+        ["index3.html#https://video-auth7.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8xNS8yMDI2IDEyOjEzOjM2IEFNJmhhc2hfdmFsdWU9TXJkVXVaSm8wa2tkWjd5SEhGbDZHUT09JnZhbGlkbWludXRlcz0xNDQwJmlkPThlZWRjOGIyLWU3MjktNGY2My04ZjFiLWVlY2IzNTNjZDM1Nw==", "https://pestech28.github.io/back/digivision/v3r5054/ch6.png"],
+        ["index3.html#https://video-auth7.iol.pt/live_tvi_reality/live_tvi_reality/edge_servers/tvireality-720_passthrough/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8xNS8yMDI2IDEyOjEzOjM2IEFNJmhhc2hfdmFsdWU9TXJkVXVaSm8wa2tkWjd5SEhGbDZHUT09JnZhbGlkbWludXRlcz0xNDQwJmlkPThlZWRjOGIyLWU3MjktNGY2My04ZjFiLWVlY2IzNTNjZDM1Nw==", "https://pestech28.github.io/back/digivision/v3r5054/ch7.png"],
+        ["index3.html#https://video-auth7.iol.pt/live_vmais/live_vmais/edge_servers/vmais-720p/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8xNS8yMDI2IDEyOjEzOjM2IEFNJmhhc2hfdmFsdWU9TXJkVXVaSm8wa2tkWjd5SEhGbDZHUT09JnZhbGlkbWludXRlcz0xNDQwJmlkPThlZWRjOGIyLWU3MjktNGY2My04ZjFiLWVlY2IzNTNjZDM1Nw==", "https://pestech28.github.io/back/digivision/v3r5054/ch8.png"],
+        ["index2.html#https://pluto.tv/us/watch/live-tv/#18785", "https://pestech28.github.io/back/digivision/v3r5054/ch9.png"],
+        ["index2.html#https://www.newsnationnow.com/news-nation-live/", "https://pestech28.github.io/back/digivision/v3r5054/ch10.png"],
+        ["index2.html#https://i1.weatherstar.dev/", "https://pestech28.github.io/back/digivision/v3r5054/ch11.png"],
+        ["index3.html#https://retroblast.tv/hls/stream.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch12.png"],
+        ["index3.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-xHCbpk-playlist.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch13.png"],
+        ["index2.html#https://pluto.tv/us/watch/live-tv/#18785", "https://pestech28.github.io/back/digivision/v3r5054/ch14.png"],
+        ["index2.html#https://pluto.tv/us/watch/live-tv/#31215", "https://pestech28.github.io/back/digivision/v3r5054/ch15.png"],
+        ["index2.html#https://pestech28.github.io/back/digivision/v3r5054/yt/tao.html", "https://pestech28.github.io/back/digivision/v3r5054/ch16.png"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
 
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["RTP 1", "Conteúdo geral"],
+        ["RTP 2", "Conteúdo variado"],
+        ["RTP Notícias", "Notícias 24 horas por dia, 7 dias por semana."],
+        ["SIC", "Conteúdo geral"],
+        ["TVI", "Conteúdo geral"],
+        ["TVI Ficção", "Conteúdo geral"],
+        ["TVI Reality", "Conteúdo geral"],
+        ["V+", "Conteúdo geral"],
+        ["Bob Ross", "Joy of Painting"],
+        ["The Hill", "Non bias news 24/7"],
+        ["Weather Scan", "Weather info 24/7"],
+        ["Cartoon Network", "Cartoons"],
+        ["Toonami Aftermath", "Cartoons and Anime"],
+        ["00s Replay", "Movies from the new mellenium"],
+        ["Vevo 2k", "Music from the new mellenium"],
+        ["Zen Music", "Traditional Asian music 24/7"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
+
+function getwebchannels() {
+    let channeldb = [
+        ["https://sweet.tv/sk/tv/847-jednotka-hd", "https://pestech28.github.io/back/digivision/v3r5054/ch1.png"],
+        ["https://sweet.tv/sk/tv/848-dvojka-hd", "https://pestech28.github.io/back/digivision/v3r5054/ch2.png"],
+        ["https://media.joj.sk/embed/3Yz1gBQeKTs?autoplay=1", "https://pestech28.github.io/back/digivision/v3r5054/ch3.png"],
+        ["https://media.joj.sk/embed/DKZwKhUlv3r5054i?autoplay=1", "https://pestech28.github.io/back/digivision/v3r5054/ch4.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch5.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/jojko-1080.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch6.png"],
+        ["https://media.cms.markiza.sk/embed/markiza-live?autoplay=any", "https://pestech28.github.io/back/digivision/v3r5054/ch7.png"],
+        ["https://media.cms.markiza.sk/embed/doma-live?autoplay=any", "https://pestech28.github.io/back/digivision/v3r5054/ch8.png"],
+        ["https://media.cms.markiza.sk/embed/dajto-live?autoplay=any", "https://pestech28.github.io/back/digivision/v3r5054/ch9.png"],
+        ["https://media.cms.markiza.sk/embed/klasik-live?autoplay=any", "https://pestech28.github.io/back/digivision/v3r5054/ch10.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/cs_history-1080.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch11.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/cs_film-1080.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch12.png"],
+        ["index2.html#https://live.cdn.joj.sk/live/andromeda/cs_mystery-1080.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch13.png"],
+        ["https://www.ta3.com/live", "https://pestech28.github.io/back/digivision/v3r5054/ch14.png"],
+        ["https://pluto.tv/us/watch/live-tv/#18785", "https://pestech28.github.io/back/digivision/v3r5054/ch15.png"],
+        ["https://pestech28.github.io/back/digivision/v3r5054/yt/thehill.html", "https://pestech28.github.io/back/digivision/v3r5054/ch16.png"],
+        ["https://i1.weatherstar.dev/", "https://pestech28.github.io/back/digivision/v3r5054/ch17.png"],
+        ["index2.html#https://retroblast.tv/hls/stream.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch18.png"],
+        ["index2.html#https://asp7.toonamiaftermath.com/livehttporigin/est/Ibd7c5-xHCbpk-playlist.m3u8", "https://pestech28.github.io/back/digivision/v3r5054/ch19.png"],
+        ["https://pluto.tv/us/watch/live-tv/#18785", "https://pestech28.github.io/back/digivision/v3r5054/ch20.png"],
+        ["https://pluto.tv/us/watch/live-tv/#31215", "https://pestech28.github.io/back/digivision/v3r5054/ch21.png"],
+        ["https://pestech28.github.io/back/digivision/v3r5054/yt/tao.html", "https://pestech28.github.io/back/digivision/v3r5054/ch22.png"],
+    ];
+    return channeldb;
+}
+
+async function tviget() {
+    url = "https://raw.githubusercontent.com/LITUATUI/M3UPT/refs/heads/main/M3U/TVI.m3u8";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const textData = await response.text();
+    
+    // Store it in a variable
+    console.log(textData);
+    return textData;
+  } catch (error) {
+    console.error("Failed to fetch file:", error);
+  }
+}
