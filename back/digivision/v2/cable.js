5c3def56f4c09b7b384b3fcfc1e50b9ea1e69b7d
pestech28 <pestech28@yahoo.com>
2026-09-07 16:08:24 -0400

    Add files via upload

--- back/digivision/v2/cable.js
+++ back/digivision/v2/cable.js
@@ -1,1 +1,26 @@
+﻿function getchannels() {
+    alert("Táto verzia Digivision už nie je podporovaná. Aktualizujte na najnovšiu verziu.");
+    let channeldb = [
+        ["index4.html", "NULL"],
+        ["index3.html#", "chbox.png"],
+        ["index.html", "NULL"],
+    ];
+    return channeldb;
+}
 
+function getchinfo() {
+    let chinfodb = [
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+        ["Channel Name", "Channel Description"],
+    ];
+    return chinfodb;
+}
+
+function getwebchannels() {
+    alert("Táto verzia Digivision už nie je podporovaná. Aktualizujte na najnovšiu verziu.");
+    let channeldb = [
+        ["index2.html#", "chbox.png"],
+    ];
+    return channeldb;
+}
